728c625af95da4e7d0e612fb98a4573dea11f79e
Rahmat <rahmatalmubarak35@gmail.com>
2021-12-18 21:10:40 +0700

    menambah komen error

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
 $con = mysqli_connect("localhost","root","","monitoringkolamikan");
  
-// Check connection
+// Melihat koneksi error atau tidak
 if (mysqli_connect_errno()){
 	echo "Koneksi database gagal : " . mysqli_connect_error();
 }
